cd02e5d260572dc9ff8bda6511e3086a370d902f
arpit.Jaiswal <arpit.Jaiswal@northgateps.com>
2017-10-28 15:13:34 +0530

    Task 2.0 Better Analyser Lookup

---
+++ InterviewTest.DriverData.UnitTests/AnalyserLookupTests.cs
@@ -0,0 +1,85 @@
+﻿using InterviewTest.DriverData.Analysers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTest.DriverData.UnitTests
+{
+    [TestFixture]
+    public class AnalyserLookupTests
+    {
+        [Test]
+        public void ShouldGetDeliveryDriverAnalyser()
+        {
+            //Assign
+            var analyserType = "delivery_driver";
+            //Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.AreEqual(typeof(DeliveryDriverAnalyser), analyser.GetType());
+        }
+        [Test]
+        public void ShouldGetFormulaOneDriverAnalyser()
+        {
+            //Assign
+            var analyserType = "formula_one_driver";
+            //Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.AreEqual(typeof(FormulaOneAnalyser), analyser.GetType());
+        }
+        [Test]
+        public void ShouldGetGetawayDriverAnalyser()
+        {
+            //Assign
+            var analyserType = "getaway_driver";
+            //Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.AreEqual(typeof(GetawayDriverAnalyser), analyser.GetType());
+        }
+
+        [Test]
+        public void ShouldGetDeliveryDriverWithPenaltyAnalyser()
+        {
+            //Assign
+            var analyserType = "delivery_driver_with_penalty";
+            //Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.AreEqual(typeof(DeliveryDriverAnalyser), analyser.GetType());
+        }
+        [Test]
+        public void ShouldGetFormulaOneWithPenaltyDriverAnalyser()
+        {
+            //Assign
+            var analyserType = "formula_one_driver_with_penalty";
+            //Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.AreEqual(typeof(FormulaOneAnalyser), analyser.GetType());
+        }
+        [Test]
+        public void ShouldGetGetawayDriverWithPenaltyAnalyser()
+        {
+            //Assign
+            var analyserType = "getaway_driver_with_penalty";
+            //Act
+            var analyser = AnalyserLookup.GetAnalyser(analyserType);
+            //Assert
+            Assert.AreEqual(typeof(GetawayDriverAnalyser), analyser.GetType());
+        }
+        [Test]
+        public void ShouldThrowErrorForWrongInput()
+        {
+            //Assign
+            var analyserType = "invalid";
+            //Act
+            //Assert
+            Assert.Throws(typeof(ArgumentOutOfRangeException), delegate { AnalyserLookup.GetAnalyser(analyserType); });
+        }
+    }
+}
--- InterviewTest.DriverData/AnalyserLookup.cs
+++ InterviewTest.DriverData/AnalyserLookup.cs
@@ -1,20 +1,45 @@
 using System;
 using InterviewTest.DriverData.Analysers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewTest.DriverData
 {
 	public static class AnalyserLookup
 	{
-		public static IAnalyser GetAnalyser(string type)
-		{
-			switch (type)
-			{
-				case "friendly":
-					return new FriendlyAnalyser();
+        private static Dictionary<Func<string, bool>, IAnalyser> Analysers = new Dictionary<Func<string, bool>, IAnalyser>();
+        static AnalyserLookup()
+        {
+            ConfigureAnalysers();
+        }
 
-				default:
-					throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
-			}
-		}
-	}
+        /// <summary>
+        /// Configure all the analysers that are supported
+        /// </summary>
+        private static void ConfigureAnalysers()
+        {
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("friendly"), new FriendlyAnalyser());
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("delivery_driver"), new DeliveryDriverAnalyser(new AnalyserSettings { StartOfDay = new TimeSpan(9, 0, 0), EndOfDay = new TimeSpan(17, 0, 0), SpeedLimit = 30, ExceedSpeedLimitRating = 0 }));
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("delivery_driver_with_penalty"), new DeliveryDriverAnalyser(new AnalyserSettings { StartOfDay = new TimeSpan(9, 0, 0), EndOfDay = new TimeSpan(17, 0, 0), SpeedLimit = 30, ExceedSpeedLimitRating = 0, ApplyUnDocumentedPenaltyFlag = true, UndocumentedPenalty = 0.5m }));
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("formula_one_driver"), new FormulaOneAnalyser(new AnalyserSettings { StartOfDay = TimeSpan.Zero, EndOfDay = TimeSpan.Zero, SpeedLimit = 200, ExceedSpeedLimitRating = 1 }));
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("formula_one_driver_with_penalty"), new FormulaOneAnalyser(new AnalyserSettings { StartOfDay = TimeSpan.Zero, EndOfDay = TimeSpan.Zero, SpeedLimit = 200, ExceedSpeedLimitRating = 1, ApplyUnDocumentedPenaltyFlag = true, UndocumentedPenalty = 0.5m }));
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("getaway_driver"), new GetawayDriverAnalyser(new AnalyserSettings { StartOfDay = new TimeSpan(13, 0, 0), EndOfDay = new TimeSpan(14, 0, 0), SpeedLimit = 80, ExceedSpeedLimitRating = 1 }));
+            AnalyserLookup.RegisterAnalysers(x => x.Equals("getaway_driver_with_penalty"), new GetawayDriverAnalyser(new AnalyserSettings { StartOfDay = new TimeSpan(13, 0, 0), EndOfDay = new TimeSpan(14, 0, 0), SpeedLimit = 80, ExceedSpeedLimitRating = 1, ApplyUnDocumentedPenaltyFlag = true, UndocumentedPenalty = 0.5m }));
+        }
+
+        private static void RegisterAnalysers(Func<string, bool> evaluator, IAnalyser analyser)
+        {
+            Analysers.Add(evaluator, analyser);
+        }
+
+        public static IAnalyser GetAnalyser(string type)
+        {
+            var analyser = Analysers.FirstOrDefault(x => x.Key(type));
+            if (analyser.Key != null)
+            {
+                return analyser.Value;
+            }
+            throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
+        }
+    }
 }
